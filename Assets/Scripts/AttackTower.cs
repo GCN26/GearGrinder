6dd96ffcc2e26d36e3bbde05c2a71b783bf79c0b
GSheldon262 <gsheldon@ltu.edu>
2024-10-13 13:11:45 -0400

    Attack Basics
    Enemies that enter tower radius get added to list and are attacked based on who entered first. If an enemy leaves the radius, they get removed from the list and no longer are targeted.
    HP and damage will be added later, alongside another type of attacking tower.

--- Assets/Scripts/AttackTower.cs
+++ Assets/Scripts/AttackTower.cs
@@ -4,5 +4,47 @@ using UnityEngine;
 
 public class AttackTower : MainTower
 {
-    
+    public float attackTimer = 0;
+    public float attackTimerTarget = 1.5f;
+    public List<GameObject> targets;
+    public GameObject searchRadius;
+
+    private void Update()
+    {
+        attackTimer += Time.deltaTime;
+
+        if(attackTimer >= attackTimerTarget)
+        {
+            attackTimer = 0;
+            Destroy(targets[0]);
+        }
+
+        for(int i = 0;i< targets.Count; i++)
+        {
+            if (targets[i] == null)
+            {
+                targets.RemoveAt(i);
+            }
+        }
+
+        //Tower Ghost shows up on valid selections
+        //if grid is selected and no tower is already on that space, if click, build tower
+        if (highlighted)
+        {
+            if (!GameObject.Find("Ghost" + this.name))
+            {
+                towerGhost = Instantiate(gridManager.GetComponent<SelectionManager>().selectedTowerGhost, transform.position, transform.rotation);
+                towerGhost.name = "Ghost" + this.name;
+            }
+        }
+        if (highlighted == false)
+        {
+            Destroy(GameObject.Find("Ghost" + this.name));
+        }
+        //if hp <= 0, reset to grid space
+    }
+    private void OnDestroy()
+    {
+        targets = null;
+    }
 }
---
+++ Assets/Scripts/AttackTowerRadius.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class AttackTowerRadius : MonoBehaviour
+{
+    public GameObject towerParent;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            towerParent.GetComponent<AttackTower>().targets.Add(other.gameObject);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            towerParent.GetComponent<AttackTower>().targets.Remove(other.gameObject);
+        }
+    }
+}
